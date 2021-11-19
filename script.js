56708367fd23ef9a0a7570b26c1ff77195131559
Shishir Neupane <shishirs520@gmail.com>
2021-11-19 16:47:44 +0545

    screen reader reads paragraph content too

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
+const acc = document.getElementsByClassName("accordion");
 
-for(var i = 0; i < acc.length; i++) {
+for(let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    var para = this.nextElementSibling;
+    const para = this.nextElementSibling;
 
     if(para.style.display === "block") {
       para.style.display = "none";
